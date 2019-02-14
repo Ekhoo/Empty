bcbf38b3d63ca3696ac40483cad24d7ae2800aca
Lucas Ortis <me@lucas-ortis.com>
2019-02-14 17:49:04 +0100

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,10 +1,5 @@
 window.addEventListener("setAccessToken", receiveAccessToken, false)
-window.addEventListener("setRefreshToken", receiveRefreshToken, false)
 
-function receiveAccessToken(event) {
-  console.log("ReceiveAccessToken => " + event)
-}
-
-function receiveRefreshToken(event) {
-  console.log("ReceiveRefreshToken => " + event)
+function didReceiveMessage(event) {
+  console.log("Did receive message => " + event.data)
 }
