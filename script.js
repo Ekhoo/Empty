1a99cc87b70e1c195964316b5c8eac3ec2974b5c
Lucas Ortis <me@lucas-ortis.com>
2019-02-14 17:09:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,21 @@ var iframe
 iframe = document.createElement('iframe')
 iframe.src = 'https://privowny.com'
 iframe.style.display = 'none'
+iframe.id = "iframe_id"
 document.body.appendChild(iframe)
 
 var accessToken = document.currentScript.getAttribute('accessToken')
 var refreshToken = document.currentScript.getAttribute('refreshToken')
 
-console.log(accessToken)
-console.log(refreshToken)
+console.log("accessToken => " + accessToken)
+console.log("refreshToken => " + refreshToken)
+
+var myFrame = document.getElementById("iframe_id")
+
+myFrame.contentWindow.foo = function() {
+    console.log ("Look at me, executed inside an iframe!")
+}
+
+myFrame.onload = function(e) {
+    myFrame.contentWindow.foo()
+ }
