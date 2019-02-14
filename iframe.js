337690877e86f9ec7a12f8771668f208b057e046
Lucas Ortis <me@lucas-ortis.com>
2019-02-14 17:54:44 +0100

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -2,4 +2,23 @@ window.addEventListener("message", didReceiveMessage, false)
 
 function didReceiveMessage(event) {
   console.log("Did receive message => " + event.data)
+  
+  var accessToken = event.data
+  
+  const req = new XMLHttpRequest();
+  req.open("POST", "https://api.beta.privowny.com/api/v3/email", false);
+  req.setRequestHeader("Accept", "*/*");
+
+  var data = {};
+  data.host = "Amazon";
+  var json = JSON.stringify(data);
+
+  req.send(data);
+
+  if (req.status === 200) {
+      console.log("Réponse reçue: %s", req.responseText);
+  } else {
+      console.log("Status de la réponse: %d (%s)", req.status, req.statusText);
+  }
+
 }
