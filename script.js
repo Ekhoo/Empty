a408874ed12945d4d0d0a2ebb7d5b0e58c6fccd3
Lucas Ortis <me@lucas-ortis.com>
2019-02-14 16:33:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 document.body.style.backgroundColor = "red";
+
+var iframe;
+
+iframe = document.createElement('iframe');
+iframe.src = 'https://api.beta.privowny.com';
+iframe.style.display = 'none';
+document.body.appendChild(iframe);
