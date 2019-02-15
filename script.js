2ad93b63bd18bb71f8902085d72480b4f0f9b19c
Lucas Ortis <me@lucas-ortis.com>
2019-02-15 10:41:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ document.body.appendChild(iframe)
 
 var myFrame = document.getElementById("iframe_id")
 
-myFrame.onload = function() {
-  var doc = myFrame.contentWindow.document;
-  doc.open()
-  doc.write('\<script type="text/javascript" src="https:\/\/ekhoo.github.io\/Empty\/iframe.js">\<\/script>')
-  doc.close() 
-  
-  console.log("IFrame did load")
-}
+//myFrame.onload = function() {
+//  var doc = myFrame.contentWindow.document;
+//  doc.open()
+//  doc.write('\<script type="text/javascript" src="https:\/\/ekhoo.github.io\/Empty\/iframe.js">\<\/script>')
+//  doc.close() 
+//  
+//  console.log("IFrame did load")
+//}
 
 //myFrame.contentWindow.postMessage("setAccessToken: " + accessToken, "*")
 //myFrame.contentWindow.postMessage("setRefreshToken: " + refreshToken, "*")
