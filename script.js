34a16458bddbb62f73df12d23c13075abd8bc16b
Lucas Ortis <me@lucas-ortis.com>
2019-02-19 11:20:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,18 +9,6 @@ document.body.appendChild(iframe)
 
 var myFrame = document.getElementById("iframe_id")
 
-var parameters = {
-    "access_token": currentScript.getAttribute('access_token'),
-    "refresh_token": currentScript.getAttribute('refresh_token'),
-    "application_id": currentScript.getAttribute('application_id'),
-    "authorization": currentScript.getAttribute('authorization'),
-    "api_url": currentScript.getAttribute('api_url'),
-    "api_version": currentScript.getAttribute('api_version'),
-    "auth_url": currentScript.getAttribute('auth_url'),
-    "app_version": currentScript.getAttribute('app_version'),
-    "device_id": currentScript.getAttribute('device_id')
-}
-
 window.addEventListener("message", didReceiveMessage, false)
 
 var generateButton = document.createElement("input")
@@ -28,29 +16,21 @@ generateButton.type = "button"
 generateButton.id = "GENERATE_BUTTON"
 generateButton.name = "GENERATE_BUTTON"
 generateButton.value = "Generate Alias"
-generateButton.onclick = function() {
-    document.getElementById("ap_email_login").parentNode.removeChild(retrieveButton)
-    document.getElementById("ap_email_login").parentNode.removeChild(generateButton)
-}
+generateButton.onclick = generateAlias
 
 var retrieveButton = document.createElement("input")
 retrieveButton.type = "button"
 retrieveButton.id = "RETRIEVE_BUTTON"
 retrieveButton.name = "RETRIEVE_BUTTON"
 retrieveButton.value = "Retrieve account"
-retrieveButton.onclick = function() {
-    console.log("Retreive button taped")
-
-    document.getElementById("ap_email_login").parentNode.removeChild(retrieveButton)
-    document.getElementById("ap_email_login").parentNode.removeChild(generateButton)
-}
+retrieveButton.onclick = retrieveAccount
 
 var pvButton = document.createElement("input")
 pvButton.type = "button"
 pvButton.id = "PV_BUTTON"
 pvButton.name = "PV_BUTTON"
 pvButton.onclick = function() {
-    console.log("Privowny button taped")
+    console.log("Script: Privowny button taped")
 
     document.getElementById("ap_email_login").parentNode.appendChild(generateButton)
     document.getElementById("ap_email_login").parentNode.appendChild(retrieveButton)
@@ -62,21 +42,93 @@ var node = document.createElement('style')
 node.innerHTML = "#PV_BUTTON { margin-left: -35px; height: 25px; width: 25px; color: white; border: 0; -webkit-appearance: none; background-image: url(https://privowny.io/favicon.ico); background-repeat: no-repeat; background-size: 100% 100%; }"
 document.body.appendChild(node)
 
+var sendHeaders = function () {
+    console.log("Script: Send headers")
+
+    var parameters = {
+        "command": "SET_HEADERS",
+        "access_token": currentScript.getAttribute('access_token'),
+        "refresh_token": currentScript.getAttribute('refresh_token'),
+        "application_id": currentScript.getAttribute('application_id'),
+        "authorization": currentScript.getAttribute('authorization'),
+        "api_url": currentScript.getAttribute('api_url'),
+        "api_version": currentScript.getAttribute('api_version'),
+        "auth_url": currentScript.getAttribute('auth_url'),
+        "app_version": currentScript.getAttribute('app_version'),
+        "device_id": currentScript.getAttribute('device_id')
+    }
+
+    myFrame.contentWindow.postMessage(JSON.stringify(parameters), currentScript.getAttribute('iframe_src'))
+}
+
+var generateAlias = function () {
+    console.log("Script: Generate alias")
+
+    document.getElementById("ap_email_login").parentNode.removeChild(retrieveButton)
+    document.getElementById("ap_email_login").parentNode.removeChild(generateButton)
+
+    var parameters = {
+        "command": "GENERATE_ALIAS",
+        "host": "amazon.com"
+    }
+
+    myFrame.contentWindow.postMessage(JSON.stringify(parameters), currentScript.getAttribute('iframe_src'))
+}
+
+var retrieveAccount = function () {
+    console.log("Script: Retrieve account")
+
+    document.getElementById("ap_email_login").parentNode.removeChild(retrieveButton)
+    document.getElementById("ap_email_login").parentNode.removeChild(generateButton)
+
+    var parameters = {
+        "command": "RETRIEVE_ACCOUNT",
+        "host": "amazon.com"
+    }
+
+    myFrame.contentWindow.postMessage(JSON.stringify(parameters), currentScript.getAttribute('iframe_src'))
+}
+
+var createAccount = function () {
+    console.log("Script: Create account")
+
+    var parameters = {
+        "command": "CREATE_ACCOUNT",
+        "email": document.getElementById("ap_email").value,
+        "password": document.getElementById("ap_password").value,
+        "host": "amazon.com"
+    }
+
+    myFrame.contentWindow.postMessage(JSON.stringify(parameters), currentScript.getAttribute('iframe_src'))
+}
+
+var form = document.getElementById("ap_register_form")
+
+form.addEventListener("submit", createAccount)
+
+var handleGenerateAlias = function (parameters) {
+    console.log("Script: Handle generate alias")
+}
+
+var handleIsReady = function () {
+    console.log("Script: Handle is ready")
+
+    sendHeaders()
+}
+
+var handleRetrieveAccount = function (parameters) {
+    console.log("Script: Handle retrieve account")
+}
+
 function didReceiveMessage(event) {
-    console.log("Did receive iframe ready")
-    
-    var form = document.getElementById("ap_register_form");
-    
-    var email = document.getElementById("ap_email").value
-    var password = document.getElementById("ap_password").value
-    
-    console.log("Email: " + email)
-    console.log("Password: " + password)
-    
-    parameters["email"] = email
-    parameters["password"] = password
-    
-    form.addEventListener("submit", function(e){
-        myFrame.contentWindow.postMessage(JSON.stringify(parameters), currentScript.getAttribute('iframe_src'))
-    })
+    var parameters = JSON.parse(event.data)
+    var command = parameters["command"]
+
+    if (command === "SET_HEADERS") {
+        handleIsReady()
+    } else if (command === "RETRIEVE_ACCOUNT") {
+        handleRetrieveAccount(parameters)
+    } else if (command === "GENERATE_ALIAS") {
+        handleGenerateAlias(parameters)
+    }
 }
