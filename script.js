50c1bfb4e3005cc31dc7e6bfd9a16ef06e6edac8
Lucas Ortis <me@lucas-ortis.com>
2019-02-19 16:48:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,135 +1,105 @@
-var currentScript = document.currentScript
-
-var iframe = document.createElement('iframe')
-iframe.src = currentScript.getAttribute('iframe_src')
-iframe.id = "iframe_id"
-iframe.style.display = 'none'
-
-document.body.appendChild(iframe)
-
-var myFrame = document.getElementById("iframe_id")
-
 window.addEventListener("message", didReceiveMessage, false)
 
-var generateAlias = function () {
-    console.log("Script: Generate alias")
+var headers = null
 
-    document.getElementById("ap_email").parentNode.removeChild(generateButton)
+window.onload = function() {
+    console.log("Frame: Is loaded")
 
     var parameters = {
+        "command": "SET_HEADERS"
+    }
+
+    parent.postMessage(JSON.stringify(parameters), "*")
+}
+
+var createAccount = function (parameters) {
+    console.log("Frame: Create account")
+
+    var parameters = JSON.parse(event.data)
+
+    const endpoint = headers["api_url"] + "/api/" + headers["api_version"] + "/manager/account"
+    const request = new XMLHttpRequest()
+    request.open("POST", endpoint, false)
+
+    request.setRequestHeader("Accept", "*/*")
+    request.setRequestHeader("Content-Type", "application/json")
+
+    request.setRequestHeader("Authorization", headers["authorization"])
+
+    var data = {
+        "host": parameters["host"],
+        "username": parameters["username"],
+        "password": parameters["password"],
+        "passwordScore": parameters["passwordScore"]
+    }
+
+    var json = JSON.stringify(data)
+
+    request.send(json)
+}
+
+var generateAlias = function (parameters) {
+    console.log("Frame: Generate alias")
+
+    var parameters = JSON.parse(event.data)
+
+    const endpoint = headers["api_url"] + "/api/" + headers["api_version"] + "/email"
+    const request = new XMLHttpRequest()
+    request.open("POST", endpoint, false)
+
+    request.setRequestHeader("Accept", "*/*")
+    request.setRequestHeader("Content-Type", "application/json")
+
+    request.setRequestHeader("Authorization", headers["authorization"])
+
+    var data = {
+        "host": parameters["host"]
+    }
+
+    var json = JSON.stringify(data)
+
+    request.send(json)
+
+    var alias = JSON.parse(request.responseText)["data"]["email"]
+
+    var response = {
         "command": "GENERATE_ALIAS",
-        "host": "amazon.com"
+        "alias": alias
     }
 
-    myFrame.contentWindow.postMessage(JSON.stringify(parameters), currentScript.getAttribute('iframe_src'))
+    parent.postMessage(JSON.stringify(response), "*")
 }
 
-var retrieveAccount = function () {
-    console.log("Script: Retrieve account")
+var retrieveAccount = function (parameters) {
+    console.log("Frame: Retrieve account")
 
-    document.getElementById("ap_email_login").parentNode.removeChild(retrieveButton)
+    var parameters = JSON.parse(event.data)
 
-    var parameters = {
+    const endpoint = headers["api_url"] + "/api/" + headers["api_version"] + "/manager/account/data?max=1000&offset=0"
+    const request = new XMLHttpRequest()
+    request.open("GET", endpoint, false)
+
+    request.setRequestHeader("Accept", "*/*")
+    request.setRequestHeader("Content-Type", "application/json")
+
+    request.setRequestHeader("Authorization", headers["authorization"])
+
+    request.send(null)
+
+    var username = JSON.parse(request.responseText)["data"]["accounts"]["Amazon"][0]["username"]
+
+    var response = {
         "command": "RETRIEVE_ACCOUNT",
-        "host": "amazon"
+        "username": username
     }
 
-    myFrame.contentWindow.postMessage(JSON.stringify(parameters), currentScript.getAttribute('iframe_src'))
+    parent.postMessage(JSON.stringify(response), "*")
 }
 
-var generateButton = document.createElement("input")
-generateButton.type = "button"
-generateButton.id = "GENERATE_BUTTON"
-generateButton.name = "GENERATE_BUTTON"
-generateButton.value = "Generate Alias"
-generateButton.onclick = generateAlias
+var setHeaders = function (parameters) {
+    console.log("Frame: Set headers")
 
-var retrieveButton = document.createElement("input")
-retrieveButton.type = "button"
-retrieveButton.id = "RETRIEVE_BUTTON"
-retrieveButton.name = "RETRIEVE_BUTTON"
-retrieveButton.value = "Retrieve account"
-retrieveButton.onclick = retrieveAccount
-
-var signInButton = document.createElement("input")
-signInButton.type = "button"
-signInButton.id = "PV_BUTTON"
-signInButton.name = "PV_BUTTON"
-signInButton.onclick = function() {
-    console.log("Script: Privowny sign in button taped")
-
-    document.getElementById("ap_email_login").parentNode.appendChild(retrieveButton)
-}
-
-var signUpButton = document.createElement("img")
-signUpButton.id = "PV_BUTTON"
-signUpButton.setAttribute("src", "https://privowny.io/favicon.ico")
-signUpButton.onclick = function() {
-    console.log("Script: Privowny sign up button taped")
-
-    document.getElementById("ap_email").parentNode.appendChild(generateButton)
-}
-
-document.getElementById("ap_email_login").parentNode.appendChild(signInButton)
-document.getElementById("ap_email").parentNode.appendChild(signUpButton)
-
-var node = document.createElement('style')
-node.innerHTML = "#PV_BUTTON { background-image: url(https://privowny.io/favicon.ico); vertical-align: middle; margin-left: -35px; height: 25px; width: 25px; color: white; border: 0; -webkit-appearance: none; background-repeat: no-repeat; background-size: 100% 100%; }"
-document.body.appendChild(node)
-
-var sendHeaders = function () {
-    console.log("Script: Send headers")
-
-    var parameters = {
-        "command": "SET_HEADERS",
-        "access_token": currentScript.getAttribute('access_token'),
-        "refresh_token": currentScript.getAttribute('refresh_token'),
-        "application_id": currentScript.getAttribute('application_id'),
-        "authorization": currentScript.getAttribute('authorization'),
-        "api_url": currentScript.getAttribute('api_url'),
-        "api_version": currentScript.getAttribute('api_version'),
-        "auth_url": currentScript.getAttribute('auth_url'),
-        "app_version": currentScript.getAttribute('app_version'),
-        "device_id": currentScript.getAttribute('device_id')
-    }
-
-    myFrame.contentWindow.postMessage(JSON.stringify(parameters), currentScript.getAttribute('iframe_src'))
-}
-
-var createAccount = function () {
-    console.log("Script: Create account")
-
-    var parameters = {
-        "command": "CREATE_ACCOUNT",
-        "username": document.getElementById("ap_email").value,
-        "password": document.getElementById("ap_password").value,
-        "host": "amazon.com",
-        "passwordScore": "STRONG"
-    }
-
-    myFrame.contentWindow.postMessage(JSON.stringify(parameters), currentScript.getAttribute('iframe_src'))
-}
-
-var form = document.getElementById("ap_register_form")
-
-form.addEventListener("submit", createAccount)
-
-var handleGenerateAlias = function (parameters) {
-    console.log("Script: Handle generate alias")
-
-    document.getElementById("ap_email").value = parameters["alias"]
-}
-
-var handleIsReady = function () {
-    console.log("Script: Handle is ready")
-
-    sendHeaders()
-}
-
-var handleRetrieveAccount = function (parameters) {
-    console.log("Script: Handle retrieve account")
-
-    document.getElementById("ap_email_login").value = parameters["username"]
+    headers = parameters
 }
 
 function didReceiveMessage(event) {
@@ -137,10 +107,12 @@ function didReceiveMessage(event) {
     var command = parameters["command"]
 
     if (command === "SET_HEADERS") {
-        handleIsReady()
+        setHeaders(parameters)
     } else if (command === "RETRIEVE_ACCOUNT") {
-        handleRetrieveAccount(parameters)
+        retrieveAccount(parameters)
     } else if (command === "GENERATE_ALIAS") {
-        handleGenerateAlias(parameters)
+        generateAlias(parameters)
+    } else if (command === "CREATE_ACCOUNT") {
+        createAccount(parameters)
     }
 }
