e0eb8861cc990b27410ede7c9778b28fc551ad45
Lucas Ortis <me@lucas-ortis.com>
2019-02-21 15:02:58 +0100

    Update mac_script.js

--- mac_script.js
+++ mac_script.js
@@ -14,7 +14,7 @@ window.addEventListener("message", didReceiveMessage, false)
 function generateAlias(e) {
     console.log("Script: Generate alias")
 
-    e.stopPropagation()
+    e.preventDefault()
     
     document.getElementById("ap_email").parentNode.removeChild(generateButton)
 
@@ -29,7 +29,7 @@ function generateAlias(e) {
 function retrieveAccount(e) {
     console.log("Script: Retrieve account")
 
-    e.stopPropagation()
+    e.preventDefault()
     
     document.getElementById("ap_email").parentNode.removeChild(retrieveButton)
 
